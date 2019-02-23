7046cd3aad0f9bd31d3f8de49b29160bdbb71e16
Hakim Ghods <ghods.hakim@hotmail.com>
2019-02-23 14:41:18 +0330

    fix

---
+++ PeopleApp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PeopleApp.Data;
+
+namespace PeopleApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly PeopleDbContext _db;
+
+        public UserController(PeopleDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Get(int page)
+        {
+            var pageSize = 10;
+
+            var skip = (page - 1) * pageSize;
+
+            var model = _db.Users.Skip(skip).Take(pageSize);
+            return Ok(model);
+        }
+    }
+}
