0ea6d595fb3e4c5e0d0d59ef95c7b5e8f6b65e2e
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-21 00:19:37 +0430

    Cancelation Token

--- PeopleApp/Pages/Index.cshtml.cs
+++ PeopleApp/Pages/Index.cshtml.cs
@@ -1,28 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using PeopleApp.Data;
 
 namespace PeopleApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger _logger;
 
-        public IndexModel(PeopleDbContext db)
+        public IndexModel(PeopleDbContext db, ILogger<IndexModel> logger)
         {
             _db = db;
+            _logger = logger;
         }
-       
+
         private readonly PeopleDbContext _db;
-     
+
+        public int MinAge { get; set; }
 
         public IList<User> UserList { get; set; }
-        public void OnGet()
+
+        public async Task<IActionResult> OnGet(int minAge,CancellationToken cancellationToken)
         {
-            UserList = _db.Users.Take(10).ToList();
+            await Task.Delay(10_000, cancellationToken);
+
+            UserList = await _db.Users.Where(u=>u.Age>=minAge).Take(10).ToListAsync(cancellationToken);
+            MinAge = minAge;
+
+            return Page();
         }
     }
 }
